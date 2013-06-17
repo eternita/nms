12cebd674fe6eff9d35337eafb3f33a46921bc74
Serega <pavlikovskiy@gmail.com>
2013-06-17 10:54:39 -0600

    + mvn build script

---
+++ n4j-ws-mvn-build.bat
@@ -0,0 +1,17 @@
+
+cd neuro4j-core
+call mvn clean install
+cd ..
+
+cd neuro4j-storage-solr
+call mvn clean install
+cd ..
+
+cd neuro4j-nms-jclient
+call mvn clean install
+cd ..
+
+cd neuro4j-nms-server
+call mvn clean install
+cd ..
+
