394d89645d7e8456992e66abe8cbb2ba5cda7462
Serega <pavlikovskiy@gmail.com>
2013-10-07 11:33:03 -0600

    change version 1.3.0-SNAPSHOT -> 1.3.0

--- neuro4j-nms-server/src/main/console/org/neuro4j/web/console/controller/SettingsController.java
+++ neuro4j-nms-server/src/main/console/org/neuro4j/web/console/controller/SettingsController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.neuro4j.nms.server.NMSServerConfig;
 import org.neuro4j.storage.Storage;
-import org.neuro4j.web.console.utils.RequestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,13 +30,12 @@ public class SettingsController {
 		NMSServerConfig nmsConfig = NMSServerConfig.getInstance();
 		
 		String storageName = request.getParameter("name");
-//		RequestUtils.params2attributes(request, "storageName");
 		Storage storage = nmsConfig.getStorage(storageName);
+		if (null == storage)
+			return "redirect:settings";
+		
 		request.setAttribute("storageObj", storage);
 		request.setAttribute("storage", storageName);
-//		request.setAttribute("nmsConfig", nmsConfig);
-//		Set<String> storageNames = nmsConfig.getStorageNames();
-//		request.setAttribute("storageNames", storageNames);
 
 		return "console/settings/storage";
 	}
